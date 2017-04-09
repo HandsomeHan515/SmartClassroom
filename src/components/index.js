c0504c5626fe434cf594fe7228f4d5ead92a343f
hanshuai <hanshuai@dongwu-inc.com>
2017-04-09 08:27:02 +0800

    add Message.js to components and add method about operating time

--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,5 @@
 import Header from './Header'
 import Footer from './Footer'
+import Message from './Message'
 
-export { Header, Footer }
+export { Header, Footer, Message }
--- src/method/time.js
+++ src/method/time.js
@@ -1,10 +1,19 @@
-export const getTime = (time) => {
+export const adjustTime = time => {
+  if (time < 10) {
+    return `0${time}`
+  } else {
+    return time
+  }
+}
+
+export const transformTime = time => {
   let date = new Date(time * 1000)
   let year = date.getFullYear()
-  let month = date.getMonth() + 1
-  let day = date.getDate()
-  let hour = date.getHours()
-  let minute = date.getMinutes()
-  let second = date.getSeconds()
+  let month = adjustTime(date.getMonth() + 1)
+  let day = adjustTime(date.getDate())
+  let hour = adjustTime(date.getHours())
+  let minute = adjustTime(date.getMinutes())
+  let second = adjustTime(date.getSeconds())
   return `${year}-${month}-${day} ${hour}:${minute}:${second}`
 }
+
